4ed59f2096ebb1a53ea6c025c07ce2d22b935363
M RISQI HIDAYAT <79409216+rizkihdyt6@users.noreply.github.com>
2023-05-14 15:47:02 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -4,28 +4,22 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 REPO="https://raw.githubusercontent.com/rizkihdyt6/ftp/main/"
 echo -e " [INFO] Downloading Update File"
-echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vmess${NC}"
-wget -q -O add-vmess "${REPO}vmess/add-vmess.sh"
-wget -q -O del-vmess "${REPO}vmess/del-vmess.sh"
-wget -q -O extend-vmess "${REPO}vmess/extend-vmess.sh"
-wget -q -O trialvmess "${REPO}vmess/trialvmess.sh"
-wget -q -O cek-vmess "${REPO}vmess/cek-vmess.sh"
-wget -q -O user-vmess "${REPO}vmess/user-vmess.sh"
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Main Menu${NC}"
+wget -q -O vmess "${REPO}menu/vmess.sh" && chmod +x vmess
+wget -q -O bot "${REPO}menu/bot.sh" && chmod +x bot
+echoo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vmess${NC}"
+wget -q -O add-vmess "${REPO}vmess/add-vmess.sh" && chmod +x add-vmess
+wget -q -O del-vmess "${REPO}vmess/del-vmess.sh" && chmod +x del-vmess
+wget -q -O extend-vmess "${REPO}vmess/extend-vmess.sh" && chmod +x extend-vmess
+wget -q -O trialvmess "${REPO}vmess/trialvmess.sh" && chmod +x trialvmess
+wget -q -O cek-vmess "${REPO}vmess/cek-vmess.sh" && chmod +x cek-vmess
+wget -q -O user-vmess "${REPO}vmess/user-vmess.sh" && chmod +x user-vmess
 sleep 0.5
 echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Bot Notif${NC}"
 wget -q -O add-bot-notif "${REPO}bot-notif/add-bot-notif.sh"
 wget -q -O del-bot-notif "${REPO}bot-notif/del-bot-notif.sh"
 sleep 0.5
-sleep 2
-chmod +x add-vmess
-chmod +x del-vmess
-chmod +x extend-vmess
-chmod +x trialvmess
-chmod +x cek-vmess
-chmod +x del-bot-notif
-chmod +x add-bot-notif
-chmod +x bot
-
+sleep q
 echo -e " [INFO] Update Successfully"
 rm -rf update.sh
 sleep 2
